38591c0a31471c5e402799fc1e15b31809057173
Yrsa-75 <73591417+Yrsa-75@users.noreply.github.com>
2026-03-04 11:38:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'i.ytimg.com' },
-      { protocol: 'https', hostname: 'yt3.ggpht.com' },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  images: { unoptimized: true },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 }
 module.exports = nextConfig
